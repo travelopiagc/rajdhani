d4b15f093839b58ff8b9940b9b5712c2e7fb3920
Anand Chitipothu <anandology@gmail.com>
2022-09-08 18:42:58 +0530

    Added search page

--- train_ride/app.py
+++ train_ride/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
 from . import config
 from . import db
 
@@ -10,6 +10,32 @@ def index():
     return render_template("index.html", config=config)
 
 @app.route("/api/stations")
-def stations():
+def api_stations():
     q = request.args.get("q")
-    return db.search_stations(q)
+    stations = db.search_stations(q)
+    return jsonify(stations)
+
+@app.route("/api/search")
+def api_search():
+    from_station = request.args.get("from")
+    to_station = request.args.get("to")
+    ticket_class = request.args.get("class")
+    date = request.args.get("date")
+
+    trains = db.search_trains(from_station, to_station, ticket_class, date)
+    return jsonify(trains)
+
+@app.route("/search")
+def search():
+    from_station = request.args.get("from")
+    to_station = request.args.get("to")
+    ticket_class = request.args.get("class")
+    date = request.args.get("date")
+
+    trains = db.search_trains(from_station, to_station, ticket_class, date)
+    return render_template("search.html",
+        trains=trains,
+        from_station=from_station,
+        to_station=to_station,
+        ticket_class=ticket_class,
+        date=date)
--- train_ride/db.py
+++ train_ride/db.py
@@ -19,3 +19,42 @@ def search_stations(q):
         {"code": "NDLS", "name": "New Delhi"},
         {"code": "MMCT", "name": "Mumbai"}
     ]
+
+def search_trains(from_station, to_station, date, ticket_class):
+    """Returns all the trains that source to destination stations on
+    the given date. When ticket_class is provided, this should return
+    only the trains that have that ticket class.
+
+    This is used to get show the trains on the search results page.
+    """
+    # TODO: make a db query to get the matching trains
+    # and replace the following dummy implementation
+
+    return [
+        {
+            "train_number": "12028",
+            "train_name": "Shatabdi Exp",
+            "from_station_code": "SBC",
+            "from_station_name": "Bangalore",
+            "to_station_code": "MAS",
+            "to_station_name": "Chennai",
+            "start_date": date,
+            "start_time": "06:00",
+            "end_date":  date,
+            "end_time": "11:00",
+            "duration": "05:00"
+        },
+        {
+            "train_number": "12608",
+            "train_name": "Lalbagh Exp",
+            "from_station_code": "SBC",
+            "from_station_name": "Bangalore",
+            "to_station_code": "MAS",
+            "to_station_name": "Chennai",
+            "start_date": date,
+            "start_time": "06:20",
+            "end_date":  date,
+            "end_time": "12:15",
+            "duration": "05:55"
+        },
+    ]
