bb4cc31f28d4fc31ab9eff34d7aed0cdd7a5d7b5
Anand Chitipothu <anandology@gmail.com>
2022-09-08 16:47:17 +0530

    forgot to include changes to app.py in the last commit

--- train_ride/app.py
+++ train_ride/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-
+from . import config
 
 app = Flask("train_ride")
 
 
 @app.route("/")
 def index():
-    return render_template("hello.html")
+    return render_template("index.html", config=config)
