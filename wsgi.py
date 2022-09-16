b8ad5ec679dec1bb5fb84efba89ff4f830043d0d
Anand Chitipothu <anandology@gmail.com>
2022-09-16 07:52:18 +0530

    Fixed conflict between print and cgi
    As sugggested in #14, resolved it by setting `sys.stdout = sys.stderr`.
    
    Closes #14

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,10 @@
 This is the convention used by boring-serverless to
 locate the entrypoint for an application.
 """
+
+# redirect the prints to stderr to avoid interfering with CGI
+# See https://github.com/pipalacademy/rajdhani/issues/14
+import sys
+sys.stdout = sys.stderr
+
 from rajdhani.app import app
