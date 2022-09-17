b1e8cfb384157213eb3a59b6e487375650ea43d2
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-09-17 10:40:18 +0530

    add schema

--- schema.sql
+++ schema.sql
@@ -41,3 +41,12 @@ create table schedule (
     arrival text,
     departure text
 );
+
+
+create table bookings (
+    train_number text references train(number),
+    ticket_class text,
+    passenger_name text,
+    passenger_email text,
+    departure_date text
+);
