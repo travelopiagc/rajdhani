77f58bd097a1705e076bbf68acf40f262d11bfc6
Giedrius Ceginskas <Giedrius.Ceginskas@travelopia.com>
2023-01-24 23:10:36 +0000

    Disable flags

--- rajdhani/config.py
+++ rajdhani/config.py
@@ -6,8 +6,8 @@ secret_key = os.getenv("SECRET_KEY", "helloworld")
  # Note: Please enable these flags only when the task asks you to do
 
 flag_homepage = True
-flag_ticketclass_in_search = True
-flag_search_filters = True
+flag_ticketclass_in_search = False
+flag_search_filters = False
 flag_show_schedule_link = False
 flag_bookings = False
 flag_login = False
